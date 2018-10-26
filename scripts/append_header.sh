03ff68243c29430dba8e085002fb5cb5068e7f63
jfajkowski <j.fajkowski@stud.elka.pw.edu.pl>
2018-10-27 00:48:53 +0200

    Add preliminary analysis of categories distribution

--- scripts/append_header.sh
+++ scripts/append_header.sh
@@ -6,10 +6,10 @@ train_file=./data/train_data.csv
 raw_test_file=$2
 test_file=./data/test_data.csv
 
-echo Extracting header line and writing it at the beggining of train and test files
+echo "Extracting header line and writing it at the beggining of train and test files"
 tail --lines=+2 $header_file | cut -f 1 -d ':' | tr '\n' ',' | tee $train_file $test_file 1>/dev/null
-echo "\n" | tee --append $train_file $test_file 1>/dev/null
+echo "category" | tee --append $train_file $test_file 1>/dev/null
 
-echo Appending the rest of raw files
-cat $raw_train_file >>$train_file
-cat $raw_test_file >>$test_file
+echo "Appending the rest of raw files (without dot at the end of the file)"
+sed 's/.$//' $raw_train_file >>$train_file
+sed 's/.$//' $raw_test_file >>$test_file
