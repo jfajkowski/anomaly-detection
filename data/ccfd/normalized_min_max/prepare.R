d6f58cf3b298c5ceeb6c18866ecfb3c64eec9220
unknown <motyka.tomasz.1995@gmail.com>
2018-12-12 13:42:17 +0100

    Set proper path to ccfg train and test files

--- data/ccfd/normalized_min_max/prepare.R
+++ data/ccfd/normalized_min_max/prepare.R
@@ -13,5 +13,5 @@ sample <- sample.split(data, split_ratio)
 train <- subset(data, sample == TRUE)
 test  <- subset(data, sample == FALSE)
 
-write.csv(train, file = "./data/ccfd/normalized_zero_one/train.csv", row.names = FALSE)
-write.csv(test, file = "./data/ccfd/normalized_zero_one/test.csv", row.names = FALSE)
+write.csv(train, file = "./data/ccfd/normalized_min_max/train.csv", row.names = FALSE)
+write.csv(test, file = "./data/ccfd/normalized_min_max/test.csv", row.names = FALSE)
