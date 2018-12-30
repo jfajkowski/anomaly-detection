ca23074293496b908073c5238dba086a219af233
jfajkowski <j.fajkowski@stud.elka.pw.edu.pl>
2018-12-30 14:25:40 +0100

    Use MAE and MSE as a model parameter

--- scripts/ccfd/evaluate.R
+++ scripts/ccfd/evaluate.R
@@ -20,56 +20,49 @@ model_dir <- args[1]
 
 FLAGS <- read_yaml(paste(model_dir, "/flags.yml", sep = ""))
 
-metrics = c("mae", "mse")
+test <- read.csv(file = file.path(FLAGS$data_dir, "test.csv"), header = TRUE, sep = ",", row.names = NULL)
+model <- load_model_hdf5(file.path(model_dir, paste("model.hdf5", sep = "")))
 
-for (metric in metrics) {
-  test <- read.csv(file = file.path(FLAGS$data_dir, "test.csv"), header = TRUE, sep = ",", row.names = NULL)
-  model <- load_model_hdf5(file.path(model_dir, paste("model_", metric, ".hdf5", sep = "")))
-  
-  X_test <- as.matrix(subset(test, select = -c(Class)))
-  y_test <- as.matrix(subset(test, select = c(Class)))
-  
-  X_predict <- model %>% predict(X_test)
-  if(metric == "mae"){
-    y_predict <- as.matrix(normalize_min_max(apply(abs(X_test - X_predict), 1, function(x) sum(x) / length(x))))
-    metric  
-  }
-  if(metric == "mse"){
-    y_predict <- as.matrix(normalize_min_max(apply(((X_test - X_predict)**2), 1, function(x) sum(x) / length(x))))
-    metric
-  }
+X_test <- as.matrix(subset(test, select = -c(Class)))
+y_test <- as.matrix(subset(test, select = c(Class)))
 
-  colnames(y_predict) <- c("Error")
-  
-  # Calculate ROC and AUC
-  roc_curve <- roc(as.vector(y_test), as.vector(y_predict))
-  auc(roc_curve)
-  
-  # Find optimal cutoff point
-  cutoff <- coords(roc_curve, "best", "threshold")
-  cat("Optimal cutoff point:", cutoff["threshold"], "\n")
-  confusionMatrix(table(y_predict > cutoff["threshold"], y_test == 1))
-  
-  # Plot ROC data
-  roc_df = data.frame(tpr=roc_curve$sensitivities, fpr=1-roc_curve$specificities)
-  ggplot(roc_df, aes(fpr, tpr)) + 
-    geom_line(color=rgb(0, 0, 1, alpha = 0.3)) +
-    coord_fixed() +
-    labs(title = sprintf("ROC")) + xlab("FPR") + ylab("TPR") +
-    geom_point(aes(x=1-cutoff["specificity"], y=cutoff["sensitivity"]), color = "red")
-  ggsave(file.path(model_dir, paste("roc_", metric, ".png", sep = "")))
-  
-  # Visualize data and density
-  df <- data.frame(y_test, y_predict)
-  df <- df[order(df$Error),]
-  df["Anomaly"] <- df$Class == 1
-  df["Id"] <- 1:nrow(df)
-  ggplot(df, aes(x=Id, y=Error, col=Anomaly)) + 
-    geom_point(alpha = 0.5) +
-    geom_rug()
-  ggsave(file.path(model_dir, paste("data_", metric, ".png", sep = "")))
-  ggplot(df, aes(x=Error, fill=Anomaly)) +
-    geom_density(alpha = 0.5)
-  ggsave(file.path(model_dir, paste("density_", metric, ".png", sep = "")))
-
+X_predict <- model %>% predict(X_test)
+if(FLAGS$metric == "mae"){
+  y_predict <- as.matrix(normalize_min_max(apply(abs(X_test - X_predict), 1, function(x) sum(x) / length(x))))
 }
+if(FLAGS$metric == "mse"){
+  y_predict <- as.matrix(normalize_min_max(apply(((X_test - X_predict)**2), 1, function(x) sum(x) / length(x))))
+}
+
+colnames(y_predict) <- c("Error")
+
+# Calculate ROC and AUC
+roc_curve <- roc(as.vector(y_test), as.vector(y_predict))
+auc(roc_curve)
+
+# Find optimal cutoff point
+cutoff <- coords(roc_curve, "best", "threshold")
+cat("Optimal cutoff point:", cutoff["threshold"], "\n")
+confusionMatrix(table(y_predict > cutoff["threshold"], y_test == 1))
+
+# Plot ROC data
+roc_df = data.frame(tpr=roc_curve$sensitivities, fpr=1-roc_curve$specificities)
+ggplot(roc_df, aes(fpr, tpr)) +
+  geom_line(color=rgb(0, 0, 1, alpha = 0.3)) +
+  coord_fixed() +
+  labs(title = sprintf("ROC")) + xlab("FPR") + ylab("TPR") +
+  geom_point(aes(x=1-cutoff["specificity"], y=cutoff["sensitivity"]), color = "red")
+ggsave(file.path(model_dir, paste("roc.png", sep = "")))
+
+# Visualize data and density
+df <- data.frame(y_test, y_predict)
+df <- df[order(df$Error),]
+df["Anomaly"] <- df$Class == 1
+df["Id"] <- 1:nrow(df)
+ggplot(df, aes(x=Id, y=Error, col=Anomaly)) +
+  geom_point(alpha = 0.5) +
+  geom_rug()
+ggsave(file.path(model_dir, paste("data.png", sep = "")))
+ggplot(df, aes(x=Error, fill=Anomaly)) +
+  geom_density(alpha = 0.5)
+ggsave(file.path(model_dir, paste("density.png", sep = "")))
--- scripts/ccfd/train_5_layers.R
+++ scripts/ccfd/train_5_layers.R
@@ -15,36 +15,30 @@ FLAGS <- flags(
   flag_integer("fourth_layer_units", 15),
   flag_string("encoder_activation", "relu"),
   flag_string("decoder_activation", "sigmoid"),
-  flag_string("data_dir", "./data/ccfd/raw")
+  flag_string("data_dir", "./data/ccfd/raw"),
+  flag_string("metric", "mse")
 )
-
-metrics = c("mae", "mse")
 
 train <- read.csv(file = file.path(FLAGS$data_dir, "train.csv"), header = TRUE, sep = ",", row.names = NULL)
 
 X_train <- as.matrix(subset(train, select = -c(Class)))
 
-for (metric in metrics) {
-  loss_f <- metric
-  model <- keras_model_sequential()
-  model %>%
-    layer_dense(units = FLAGS$second_layer_units, activation = FLAGS$encoder_activation, input_shape = ncol(X_train)) %>%
-    layer_dense(units = FLAGS$third_layer_units, activation = FLAGS$encoder_activation) %>%
-    layer_dense(units = FLAGS$fourth_layer_units, activation = FLAGS$encoder_activation) %>%
-    layer_dense(units = ncol(X_train), activation = FLAGS$decoder_activation)
-  
-  model %>% compile(
-    loss = loss_f,
-    optimizer = optimizer_adam(lr = 1e-03),
-    metrics = metric)
-  
-  model %>% fit(
-    x = X_train,
-    y = X_train, 
-    epochs = FLAGS$epochs,
-    batch_size = FLAGS$batch_size)
-  
-  model_name = paste("model_", metric, ".hdf5", sep = "")
-  
-  save_model_hdf5(model, paste(model_dir, "/", model_name, sep = ""))
-}
+model <- keras_model_sequential()
+model %>%
+  layer_dense(units = FLAGS$second_layer_units, activation = FLAGS$encoder_activation, input_shape = ncol(X_train)) %>%
+  layer_dense(units = FLAGS$third_layer_units, activation = FLAGS$encoder_activation) %>%
+  layer_dense(units = FLAGS$fourth_layer_units, activation = FLAGS$encoder_activation) %>%
+  layer_dense(units = ncol(X_train), activation = FLAGS$decoder_activation)
+
+model %>% compile(
+  loss = FLAGS$metric,
+  optimizer = optimizer_adam(lr = 1e-03),
+  metrics = FLAGS$metric)
+
+model %>% fit(
+  x = X_train,
+  y = X_train,
+  epochs = FLAGS$epochs,
+  batch_size = FLAGS$batch_size)
+
+save_model_hdf5(model, file.path(model_dir, "model.hdf5"))
